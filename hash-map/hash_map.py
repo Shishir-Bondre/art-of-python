907e0629f5ca5a7c33c693af06b0f5dbdbfa8fbc
shishir-bondre <shishir.bondre@narvar.com>
2020-01-01 16:52:17 +0530

    Add hash map implementation

---
+++ hash-map/hash_map.py
@@ -0,0 +1,23 @@
+from .linked_list import LinkedList
+
+
+class HashMap(LinkedList):
+
+    def __init__(self, length: int):
+        self.__length = length
+        self.__values = [0]*length
+        super().__init__()
+
+    def _hash_function(self, key: int):
+        return self.__length % key
+
+    def set(self, key: int, value: str):
+        hash_value = self._hash_function(key)
+        node = None if self.__values[hash_value] == 0 else self.__values[hash_value]
+        super()._set_node_pointer(node=node)
+        self.__values[hash_value] = super().add(value=value)
+
+    def get(self, key):
+        hash_value = self._hash_function(key)
+        super()._set_node_pointer(node=self.__values[hash_value])
+        return super().find()
